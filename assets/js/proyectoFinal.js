e015c31c8dd69f3f35e3a24e9e916500c08c2160
GuadalupeGerardoCP <guadalupe.gerardo.cp@gmail.com>
2022-05-22 13:18:34 -0500

    creating cards from JavaScript with product listing

---
+++ assets/js/proyectoFinal.js
@@ -0,0 +1,12 @@
+/*
+    ? Paso 1 crear una funcion guardarLocal() que tenga como proposito guardar los datos proporcionados por el usuario localmente en el navegador. que reciba como parametros:
+
+
+    * Dato = El dato que se quiere guardar localmente en el navegador
+
+    * llave = la referencia que va a tener que se va a hacer al dato local
+
+
+
+
+*/
---
+++ assets/js/tarea5.js
@@ -0,0 +1,135 @@
+function addItem(item){
+  const itemHTML = '<div class="slick">\n' +
+  '<div class="card">\n' +
+      '<div class="card-img"><img src="'+item.img +'" class="card-img-top" alt="image"></div>\n' + 
+      '<div class="card-info">\n' + 
+          '<p class="text-title">'+item.name+'</p>\n' +
+          '<p class="text-body">'+item.description+'</p>\n' +
+      '</div>\n' +
+      '<div class="card-footer">\n' +
+          '<span class="text-title">$499.49</span>\n'+
+          '<div class="card-button">\n'+
+              '<svg class="svg-icon" viewBox="0 0 20 20">\n'
+                  '<path\n' +
+                      'd="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z">\n' +
+                  '</path>\n' +
+                  '<path\n' +
+                      'd="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z">\n'+
+                  '</path>\n' +
+                  '<path\n'+
+                      'd="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z">\n' +
+                  '</path>\n'+
+              '</svg>\n'+
+          '</div>\n'+
+      '</div>\n'+
+  '</div>\n'+
+'</div>\n;'   
+  
+  const itemsContainer = document.getElementById("track");
+  itemsContainer.innerHTML += itemHTML;
+}
+
+addItem({
+  name: "juice",
+  img: "https://www.gs1india.org/media/Juice_pack.jpg",
+  description: "Orange and Apple juice fresh and delicious",
+});
+
+addItem({
+  name: "Tayto",
+  img: "https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg",
+  description: "Cheese & Onion Chips",
+});
+
+addItem({
+  name: "Lavadora LG",
+  img: "https://www.lg.com/cac/images/lavadoras-y-secadoras/md05898276/2_Gallery/N01_medium01.jpg",
+  description:
+    "14/10kg Lavadora Doble Tina, Filtro de Pelusa, Rejilla Anti-Readores, Color Blanco",
+});
+addItem({
+  name: "Refrigerador Samsung",
+  img: "https://www.costco.com.mx/medias/sys_master/products/h94/h23/79611664564254.webp",
+  description: "22 French Door",
+});
+addItem({
+  name: "Refrigerador Samsung",
+  img: "https://images.samsung.com/is/image/samsung mx-side-by-side-rs27t5561b1-rs27t5561b1-em-frontjetblack-203713839?$1300_1038_PNG$",
+  description:
+    "Side by Side 27 cu.ft con Family Hub y despachador de agua y hielo",
+});
+addItem({
+  name: "Banca para Jardín",
+  img: "https://cdn1.coppel.com/images/catalog/pm/3665443-1.jpg",
+  description: "Banca para Jardín Congo Delia",
+});
+addItem({
+  name: "Juego de jardin",
+  img: "https://cdn1.coppel.com/images/catalog/pm/3487413-1.jpg",
+  description: "Juego de Jardín Apollo con 2 Sillas",
+});
+addItem({
+  name: "Mesa Rectangular",
+  img: "https://cdn1.coppel.com/images/catalog/pm/3406343-1.jpg",
+  description: "Mesa Rectangular Congo Ibiza II",
+});
+addItem({
+  name: "Alberca Rectangular",
+  img: "https://cdn1.coppel.com/images/catalog/pm/5480733-1.jpg",
+  description: "Intex 2282 L 260 x 160 x 65 cm",
+});
+addItem({
+  name: "AT&T",
+  img: "https://cdn1.coppel.com/images/catalog/pm/2512073-1.jpg",
+  description: "Unefon Motorola Edge 20 Pro 256 GB Blanco",
+});
+addItem({
+  name: "Consola",
+  img: "https://cdn1.coppel.com/images/catalog/pm/2889483-1.jpg",
+  description: "Consola Xbox Series X 1 TB",
+});
+addItem({
+  name: "Consola",
+  img: "https://cdn1.coppel.com/images/catalog/pm/2889313-1.jpg",
+  description: "Consola Xbox Series S 512 GB",
+});
+addItem({
+  name: "Leche Lala",
+  img: "https://www.chedraui.com.mx/medias/7501020550986-00-CH515Wx515H?context=bWFzdGVyfHJvb3R8Mzk0MjB8aW1hZ2UvanBlZ3xoMTYvaDk5LzEyMDI3MDc2NzcxODcwLmpwZ3xmMjEwZDU0NWZiNmQ0ZmM1NmFlNjA3ZDA3MzdmNmY4ZjdhZTJmNjQ4ZjI2MjFkZWRhNzM1YmViOWM4ZjAyZjBh",
+  description: "100 Sin Lactosa Proteína 1 L",
+});
+
+/*
+<div class="card">
+        <div class="card-img"></div>
+        <div class="card-info">
+          <p class="text-title">Product title </p>
+          <p class="text-body">Product description and details</p>
+        </div>
+        <div class="card-footer">
+          <span class="text-title">$499.49</span>
+          <div class="card-button">
+            <svg class="svg-icon" viewBox="0 0 20 20">
+              <path
+                d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z">
+              </path>
+              <path
+                d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z">
+              </path>
+              <path
+                d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z">
+              </path>
+            </svg>
+          </div>
+        </div>
+      </div>
+*/
+/*'<div class="card" style="width: 18rem;">\n' +
+      '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+      '    <div class="card-body">\n' +
+      '        <h5 class="card-title">'+item.name+'</h5>\n' +
+      '        <p class="card-text">'+item.description+'</p>\n' +
+      '        <a href="#" class="btn btn-primary">Add</a>\n' +
+      '    </div>\n' +
+      '</div>\n' +
+      '<br/>'*/
