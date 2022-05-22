63847f6257b5abed80852a0ba15be6cb9e8a14f6
JC Flores <juanc.fm.1195@gmail.com>
2022-05-21 22:47:30 -0500

    Bugs and added bidding functionality

--- assets/js/tarea5.js
+++ assets/js/tarea5.js
@@ -1,15 +1,15 @@
 function addItem(item){
     const itemHTML = '<div class="slick">\n' +
     '<div class="card">\n' +
-        '<div class="card-img"></div>\n' + 
+        '<div class="card-img"><img src = "'+ item.img +'" id = "imgOferta"></div>\n' + 
         '<div class="card-info">\n' + 
-            '<p class="text-title">${item.name}</p>\n' +
-            '<p class="text-body">${item.description}</p>\n' +
+            '<p class="text-title">'+ item.name + '</p>\n' +
+            '<p class="text-body">' + item.description + '</p>\n' +
         '</div>\n' +
         '<div class="card-footer">\n' +
             '<span class="text-title">$499.49</span>\n'+
             '<div class="card-button">\n'+
-                '<svg class="svg-icon" viewBox="0 0 20 20">\n'
+                '<svg class="svg-icon" viewBox="0 0 20 20">\n'+
                     '<path\n' +
                         'd="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z">\n' +
                     '</path>\n' +
@@ -23,19 +23,8 @@ function addItem(item){
             '</div>\n'+
         '</div>\n'+
     '</div>\n'+
-'</div>\n'   
-    
-    
-    /*'<div class="card" style="width: 18rem;">\n' +
-        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">'+item.name+'</h5>\n' +
-        '        <p class="card-text">'+item.description+'</p>\n' +
-        '        <a href="#" class="btn btn-primary">Add</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>'*/;
-    const itemsContainer = document.getElementById("list-items");
+'</div>\n';
+    const itemsContainer = document.getElementById("track");
     itemsContainer.innerHTML += itemHTML;
 }
 
@@ -45,7 +34,7 @@ addItem({'name':'juice',
 
 addItem({'name':'Tayto',
     'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
-    'description':'Cheese & Onion Chips'})
+    'description':'Cheese & Onion Chips'});
 
 
 /*
