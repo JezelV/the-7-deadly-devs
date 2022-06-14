4bbf5da545f077dac5ca64e23d645f9d8dbd4619
Argemiro Haro <argeharo@outlook.es>
2022-06-13 22:37:17 -0500

    Added alerts in contact us

--- assets/js/contactUs.js
+++ assets/js/contactUs.js
@@ -4,48 +4,63 @@ Codigo para validar campos del formulario y poder enviar el JSON
 
 const inputs2 = document.querySelectorAll(".inputC");
 const btnSubmit = document.getElementById("btnSubmit");
+var entrada = [];
+const checkbox = document.getElementById("terms");
 
 //^(?!.* $)[A-Z][a-z ]+$
 btnSubmit.addEventListener('click',()=>{
   //Validacion de campo por campo
   let formValid = true;
+  if(checkbox.checked){
   for (var i=0; i<inputs2.length; i++) {
     if(inputs2[i].type =='text'){
       if (!(/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(inputs2[i].value))){
-        alert (inputs2[i].name+ ' no puede contener simbolos, estar vacío o contener sólo espacios en blanco');
+        entrada [i] = inputs2[i].name;
         formValid=false;
       }
     }
     if(inputs2[i].type =='email'){
       //Validacion de email
       if (!(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z-.]+$/.test(inputs2[i].value))){
-        alert ('Por favor ingrese un correo valido.');
+        entrada [i] = inputs2[i].name;
         formValid=false;
       }
     }
     if(inputs2[i].type =='tel'){
       //Validacion de tel
       if (!(/^\d{7,14}$/.test(inputs2[i].value))){
-        alert ('Por favor ingrese un telefono valido (este debe contener solo numeros de 7 a 14 digitos).');
+        entrada [i] = inputs2[i].name;
         formValid=false;
       }
     }
     if(inputs2[i].type =='textarea'){
       //Validacion del textarea
       if (/^\s*$/.test(inputs2[i].value)){
-        alert ('Por favor ingrese un mensaje valido (este no debe estar vacio o contener solo espacios).');
+        entrada [i] = inputs2[i].name;
         formValid=false;
       }
     }
     if(inputs2[i].id =='optionC'){
       //Validacion del selector
       if(inputs2[i].value == "-- Elige una opción por favor --"){
-        alert("Por favor selecciona una opción.");
+        entrada [i] = inputs2[i].name;
         formValid=false;
       }
     }
   }
-  
+  if(formValid==false){
+    Swal.fire({
+      icon: 'warning',
+      title: 'Verifica los campos:',
+      text: entrada.join(", "),
+    });
+  }
+}else{
+  Swal.fire({
+    icon: 'warning',
+    title: 'Verifica los terminos y condiciones',
+  });
+}
   if(formValid==true){
     //Si todos los campos estan validados correctamemnte se armara el json para poder enviarse
     localStorage.clear();
